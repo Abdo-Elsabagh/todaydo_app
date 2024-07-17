14e6ffe8afc6d486c3cf325be4628c3ed2419cf4
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-07-17 12:19:10 +0300

    String in app

---
+++ lib/screen/tasks_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal[400],
+      body: Column(
+        
+      ),
+    );
+  }
+}
